139ac40d3f25c47c42b078d40cd2e8cb00e435b2
Shubhu0500 <shubhu0500@gmail.com>
2025-04-25 19:02:14 +0530

    authcontroller mocha-chai test started

---
+++ test/auth-controller.js
@@ -0,0 +1,16 @@
+const expect = require('chai').expect;
+const sinon = require('sinon');
+
+const User = require('../models/user');
+const AuthController = require('../controllers/auth');
+
+describe('Auth Controller- Login', function () {
+  it('should throw and error with error code 500 if accessing the database fails', function () {
+    sinon.stub(User, 'findOne');
+    User.findOne.throws();
+
+    expect(AuthController.login);
+
+    User.findOne.restore();
+  });
+});
